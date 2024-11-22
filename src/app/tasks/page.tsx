8166afaf1b6bb202ffaa4b593558d866fdfffa92
Mehdi Monfared <mehdi.monfared@element.com>
2024-11-22 21:14:15 +0000

    force-dynamic

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,6 +1,8 @@
 import { getData } from "@/actions/toDoActions";
 import { Todos } from "@/components/ToDos/ToDos";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const data = await getData();
   return <Todos todos={data} />;
